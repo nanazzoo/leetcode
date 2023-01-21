5c1652f39a028438d5acc2009381cea3436d9d58
Shin_Bokyung <101784680+nanazzoo@users.noreply.github.com>
2023-01-21 09:53:35 +0900

    Time: 1 ms (99.95%), Space: 42.9 MB (79.03%) - LeetHub

---
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public int[] buildArray(int[] nums) {
+        
+        int[] arr = new int[nums.length];
+        
+        for(int i=0; i<nums.length; i++) {
+            arr[i] = nums[nums[i]];
+        }
+        
+        return arr;
+        
+    }
+}
